a65fc472b8101ddef527b13bd8f675320f90d628
SebastianHuaringa <>
2026-06-09 05:03:14 -0500

    Avance de proyecto 5:02 am Agregando controladores.

--- src/main/java/com/mycompany/proyectoventa_de_entradas/ProyectoVenta_de_Entradas.java
+++ src/main/java/com/mycompany/proyectoventa_de_entradas/ProyectoVenta_de_Entradas.java
@@ -15,22 +15,22 @@ import vista.frmAdminDashboard;
 public class ProyectoVenta_de_Entradas {
 
     public static void main(String[] args) {
-        // 1. Creamos e inicializamos las listas de datos directamente aquí en el Main
+       
         ArrayList<Concierto> conciertosIniciales = new ArrayList<>();
         ArrayList<Venta> ventasIniciales = new ArrayList<>();
 
-        // Quemamos/Agregamos los datos de prueba directamente aquí para que la tabla no aparezca vacía
+      
         ventasIniciales.add(new Venta(new Date(), 150));
         ventasIniciales.add(new Venta(new Date(System.currentTimeMillis() - 86400000), 320));
         ventasIniciales.add(new Venta(new Date(System.currentTimeMillis() - 172800000), 90));
 
-        // 2. Instanciamos la vista raíz (el Dashboard del Administrador)
+   
         frmAdminDashboard vistaDashboard = new frmAdminDashboard();
         
-        // 3. Inicializamos el controlador inyectándole las listas que acabamos de crear arriba
+  
         ControladorAdminDashboard controlador = new ControladorAdminDashboard(vistaDashboard, conciertosIniciales, ventasIniciales);
         
-        // 4. Lanzamos el flujo de la interfaz gráfica
+    
         controlador.iniciar();
     }
 }
